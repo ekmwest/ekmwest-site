9b1f127fea4e7f9511e9fcbed69ea6ef7de4cf09
Karl West <mickel.west@gmail.com>
2020-12-18 21:59:31 +0100

    removed dead code

--- deps.js
+++ deps.js
@@ -1,3 +1,2 @@
 export * as path from "https://deno.land/std@0.81.0/path/mod.ts";
 export { sideServe } from "https://cdn.jsdelivr.net/gh/ekmwest/side@3.0.0/mod.js";
-//export { sideServe } from "../side/mod.js";
