a5c9fbab37a2fc48ba4cfccb41d2a55c4a49c97c
Karl West <mickel.west@gmail.com>
2021-12-03 20:08:04 +0100

    using mediaQuery event instead of resize

--- docs/js/is-mobile.js
+++ docs/js/is-mobile.js
@@ -2,10 +2,17 @@
 // to make sure css class is set before first render.
 
 (function () {
+
     const MOBILE_WIDTH = 768;
 
-    function updateIsMobile() {
-        if (document.documentElement.clientWidth && document.documentElement.clientWidth <= MOBILE_WIDTH) {
+    var query = window.matchMedia(`(max-width: ${MOBILE_WIDTH}px)`);
+
+    setScreenMode();
+
+    query.addEventListener('change', setScreenMode);
+
+    function setScreenMode() {
+        if (query.matches) {
             window.isMobile = true;
             document.documentElement.classList.add('is-mobile');
         } else {
@@ -13,22 +20,4 @@
             document.documentElement.classList.remove('is-mobile');
         }
     }
-
-    updateIsMobile();
-
-    let timeout = null;
-
-    window.addEventListener('resize', () => {
-        const wait = 100; // 100ms debounce
-
-        if (timeout) {
-            clearTimeout(timeout);
-        }
-
-        timeout = setTimeout(() => {
-            timeout = null;
-            updateIsMobile();
-        }, wait);
-    });
-
 })();
